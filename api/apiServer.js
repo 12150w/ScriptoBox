cf676d838115226aa4f29b2bcb6fd8ac55a09aa3
wnaslund <wnaslund@starlightconsultants.com>
2014-03-23 14:19:16 -0600

    started API

---
+++ api/apiServer.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var express = require('express');
+
+var Schema = mongoose.Schema;
+var app = express();
+var db = mongoose.connect('mongodb://localhost/scriptobox');
+
+var Pad = mongoose.model('Pad', new Schema({
+	title: String,
+	active: Boolean
+}) );
+
+app.use('/api', function(req, res, next) {
+	
+	// Get params
+	var rawVars = req.path.split('/');
+	var vars = [];
+	for(var i=0; i<rawVars.length; i++) {
+		if(!!rawVars[i]) vars.push(rawVars[i]);
+	}
+	
+	res.send({vars: vars, type: req.method});
+	
+});
+
+var server = app.listen(3000, function() {
+	console.log('ScriptoBox Server started');
+});
