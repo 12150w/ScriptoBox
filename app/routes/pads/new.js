fbd5bb1d1d096663019f2974537d4a8f22fcfd4c
William Naslund <12150w@gmail.com>
2014-04-01 21:15:41 -0600

    added delete to unsaved new pad

--- app/routes/pads/new.js
+++ app/routes/pads/new.js
@@ -1,5 +1,10 @@
 export default Ember.Route.extend({
 	model: function() {
 		return this.store.createRecord('pad', {title: 'Un-Named Pad'});
+	},
+	exit: function() {
+		if(!this.get('currentModel').get('id')) {
+			this.get('currentModel').deleteRecord();
+		}
 	}
 });
